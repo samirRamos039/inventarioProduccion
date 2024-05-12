52de2fd41b045556182a8e291635fb1628a60483
rzagza039@gmail.com <rzagza039@gmail.com>
2024-05-12 16:45:12 -0500

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,97 @@
 import slicebar from "./slicebar"
 
+
 function App() {
 
   return (
     <div class="wrapper">
-      <aside id="sidebar">
-        <div class="d-flex">
-          <button id="toggle-btn" type="button">
-            <i class="lni lni-grid-alt"></i>
-          </button>
-          <div class="sidebar-logo">
-            <a href="#">CodzSword</a>
-          </div>
-        </div>
-        <ul class="sidebar-nav">
-          <li class="sidebar-item">
-            <a href="#" class="sidebar-link">
-              <i class="lni lni-users"></i>
-              <span>PROFILE</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a href="#" class="sidebar-link">
-              <i class="lni lni-calendar"></i>
-              <span>TASK</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
-              data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="lni lni-protection"></i>
-              <samp>Auth</samp>
-            </a>
-            <ul id="auth" class="sidebar-dropdown list-unstyled collapsed" data-bs-parent="sidebar">
-              <li class="sidebar-item">
-                <a href="#" class="sidebar-link">login</a>
-              </li>
-              <li class="sidebar-item">
-                <a href="#" class="sidebar-link">register</a>
-              </li>
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class="lni lni-grid-alt"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">CodzSword</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Task</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                        <i class="lni lni-protection"></i>
+                        <span>Auth</span>
+                    </a>
+                    <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Login</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Register</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+                        <i class="lni lni-layout"></i>
+                        <span>Multi Level</span>
+                    </a>
+                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                                Two Links
+                            </a>
+                            <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 1</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-popup"></i>
+                        <span>Notification</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-cog"></i>
+                        <span>Setting</span>
+                    </a>
+                </li>
             </ul>
-          </li>
-          <li class="sidebar-item">
-            <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
-              data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
-              <i class="lni lni-layout"></i>
-              <samp>Multilevel</samp>
-            </a>
-            <ul id="multi" class="sidebar-dropdown list-unstyled collapsed" data-bs-parent="sidebar">
-              <li className="sidebar-item">
-                <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
-                  data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                  Tow links
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Logout</span>
                 </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-
-      </aside>
+            </div>
+        </aside>
+        <div class="main p-3">
+            <div class="text-center">
+                <h1>
+                    Sidebar Bootstrap 5
+                </h1>
+            </div>
+        </div>
     </div>
   )
 }
