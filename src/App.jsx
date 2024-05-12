64ebacdb3df488785ecfef65d0f47a8e125fd6c0
rzagza039@gmail.com <rzagza039@gmail.com>
2024-05-12 06:44:24 -0500

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import slicebar from "./slicebar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div class="wrapper">
+      <aside id="sidebar">
+        <div class="d-flex">
+          <button id="toggle-btn" type="button">
+            <i class="lni lni-grid-alt"></i>
+          </button>
+          <div class="sidebar-logo">
+            <a href="#">CodzSword</a>
+          </div>
+        </div>
+        <ul class="sidebar-nav">
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">
+              <i class="lni lni-users"></i>
+              <span>PROFILE</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">
+              <i class="lni lni-calendar"></i>
+              <span>TASK</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
+              data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="lni lni-protection"></i>
+              <samp>Auth</samp>
+            </a>
+            <ul id="auth" class="sidebar-dropdown list-unstyled collapsed" data-bs-parent="sidebar">
+              <li class="sidebar-item">
+                <a href="#" class="sidebar-link">login</a>
+              </li>
+              <li class="sidebar-item">
+                <a href="#" class="sidebar-link">register</a>
+              </li>
+            </ul>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
+              data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+              <i class="lni lni-layout"></i>
+              <samp>Multilevel</samp>
+            </a>
+            <ul id="multi" class="sidebar-dropdown list-unstyled collapsed" data-bs-parent="sidebar">
+              <li className="sidebar-item">
+                <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse"
+                  data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                  Tow links
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+      </aside>
+    </div>
   )
 }
 
---
+++ src/slicebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function slicebar() {
+    return <h1>
+        SLICEBAR
+    </h1>
+
+
+}
+export default slicebar
